45cb5904a59653adcf4b82e9a6be0cfc9d995908
Aiham Hamoudh <aiham5hamoudh@gmail.com>
2025-03-14 18:32:48 -0500

    finished-puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react";
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log(puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Puppy Bowl</h1>
+
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+            <li>Tricks:</li>
+            <ul>
+              {featuredPup.tricks.length > 0 ? (
+                featuredPup.tricks.map((trick) => (
+                  <li key={trick.id}>{trick.title}</li>
+                ))
+              ) : (
+                <li>No tricks yet!</li>
+              )}
+            </ul>
+          </ul>
+        </div>
+      )}
+
+      {puppies.map((pup) => (
+        <p
+          key={pup.id}
+          onClick={() => setFeatPupId(pup.id)}
+          style={{ cursor: "pointer" }}
+        >
+          {pup.name}
+        </p>
+      ))}
     </>
   );
 }
